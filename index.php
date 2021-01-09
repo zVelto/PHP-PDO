3efa19aa141abad97f113ec07c0633dd8c0de84e
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-09 15:36:16 -0300

    PDOStatement Object (Query) com fetchAll

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
-    $usuario = 'root';
-    $senha = '';
+$dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+$usuario = 'root';
+$senha = '';
 
-    try{
-        $conexao = new PDO($dsn, $usuario , $senha);
-        
+try {
+    $conexao = new PDO($dsn, $usuario, $senha);
+    /*
         $query = '
             create table tb_usuarios(
                     id int not null primary key auto_increment,
@@ -18,25 +18,54 @@
         $retorno = $conexao->exec($query); //retorno 0, pois nao estamos modificando registros
 
         echo $retorno;
+        */
+    // $query = '
+    //             insert into tb_usuarios(
+    //                 nome, email, senha 
+    //             ) values (
+    //                 "Jorge Sant Ana", "jorge@teste.com.br", "123456"
+    //             )
+    // ';
 
-        // $query = '
-        //         insert into tb_usuarios(
-        //             nome, email, senha 
-        //         ) values (
-        //             "Jorge Sant Ana", "jorge@teste.com.br", "123456"
-        //         )
-        // ';
-        $query = '
-                delete from tb_usuarios
-        ';
+    // $conexao->exec($query);
 
-        $retorno = $conexao->exec($query); //vai retornar 1
-        echo $retorno;
-    } catch(PDOException $e) {
-        // echo '<pre>';
-        //     print_r($e);
-        // echo '</pre>';
+    // $query = '
+    //             insert into tb_usuarios(
+    //                 nome, email, senha 
+    //             ) values (
+    //                 "Jamilton Damasceno", "jamilton@teste.com.br", "456789"
+    //             )
+    // ';
+    // $conexao->exec($query);
 
-        echo 'Erro: '. $e->getCode(). ' Mensagem: '. $e->getMessage();
-        //registrar o erro de alguma forma.
-    }
+    // $query = '
+    //     insert into tb_usuarios(
+    //         nome, email, senha 
+    //     ) values (
+    //         "Maria Silva", "maria@teste.com.br", "456123"
+    //     )
+    // ';
+    // $conexao->exec($query);
+
+
+    $query = '
+        select * from tb_usuarios
+    ';
+
+    $stmt = $conexao->query($query); //PDO Statemet
+    $lista = $stmt->fetchAll();
+
+    echo '<pre>';
+        print_r($lista);
+    echo '</pre>';
+
+    echo $lista[2]['email'];
+
+} catch (PDOException $e) {
+    // echo '<pre>';
+    //     print_r($e);
+    // echo '</pre>';
+
+    echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage();
+    //registrar o erro de alguma forma.
+}
