9ee8c4d1ac07e506ebb8605a290df6435d497432
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-08 21:40:21 -0300

    Tratando exceptions (PDOException)

--- index.php
+++ index.php
@@ -4,4 +4,13 @@
     $usuario = 'root';
     $senha = '';
 
-    $conexao = new PDO($dsn, $usuario , $senha);
+    try{
+        $conexao = new PDO($dsn, $usuario , $senha);
+    } catch(PDOException $e) {
+        // echo '<pre>';
+        //     print_r($e);
+        // echo '</pre>';
+
+        echo 'Erro: '. $e->getCode(). ' Mensagem: '. $e->getMessage();
+        //registrar o erro de alguma forma.
+    }
